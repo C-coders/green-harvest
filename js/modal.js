84875949c68ebdd45b894d64f6c391e6e32bb6b9
berkanyilmaz <sby.notess@gmail.com>
2024-12-02 02:32:14 +0300

    homework 6

--- js/modal.js
+++ js/modal.js
@@ -3,14 +3,19 @@ const refs = {
   closeModalBtn: document.querySelector('[data-close-modal]'),
   modalBg: document.querySelector('[data-modal-bg]'),
 };
+document.body.classList.remove('modal-open');
 
 refs.openModalBtn.addEventListener('click', toggleModal);
 refs.closeModalBtn.addEventListener('click', toggleModal);
 refs.modalBg.addEventListener('click', handleModalBgClick);
 
 function toggleModal() {
-  refs.modalBg.classList.toggle('is-hidden');
-  document.body.classList.toggle('modal-open');
+   const isHidden = refs.backdrop.classList.toggle('is-hidden');
+  if (isHidden) {
+    document.body.classList.remove('modal-open');
+  } else {
+    document.body.classList.add('modal-open');
+  }
 }
 
 function handleModalBgClick(event) {
